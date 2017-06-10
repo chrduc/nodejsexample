b70f8b7e36bbcea387aa9cd9add6e908dcf89a2c
DUCK-DESKTOP\chrduc <chris.duck@gmail.com>
2017-06-10 21:59:40 +1000

    cleanup

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ http.createServer(function (req, res) {
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+      res.write(uc("Hello World!"));
+      return res.end();
     }  
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
-    res.write(uc("Hello World!"));
     return res.end();
   });
 }).listen(8080);
